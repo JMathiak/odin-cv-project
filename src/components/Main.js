96adf25c7e5277f0c3e6eb582ded9ddcec626c08
Josh Mathiak <jmathiak4@gmail.com>
2022-06-20 19:24:41 -0500

    Added other 2 buttons for personal form + on click events

--- src/components/Main.js
+++ src/components/Main.js
@@ -20,6 +20,8 @@ class Main extends Component {
     };
     this.onChange = this.onChange.bind(this);
     this.onClick = this.onClick.bind(this);
+    this.onClickPersonalEdit = this.onClickPersonalEdit.bind(this);
+    this.onClickPersonalDelete = this.onClickPersonalDelete.bind(this);
   }
 
   onChange(e) {
@@ -29,20 +31,59 @@ class Main extends Component {
       [e.target.name]: n,
     });
   }
+
+  onClickPersonalEdit(e) {
+    e.preventDefault();
+    this.setState({
+      name: this.state.cv.name,
+      email: this.state.cv.email,
+      phone: this.state.cv.phone,
+    });
+  }
+
+  onClickPersonalDelete(e) {
+    e.preventDefault();
+    this.setState({
+      name: "",
+      email: "",
+      phone: "",
+    });
+    this.setState((prevState) => {
+      let cv = Object.assign({}, prevState.cv);
+      cv.name = this.state.name;
+      cv.email = this.state.email;
+      cv.phone = this.state.phone;
+      return { cv };
+    });
+  }
   onClick(e) {
     e.preventDefault();
     this.setState((prevState) => {
       let cv = Object.assign({}, prevState.cv);
       cv.name = this.state.name;
       cv.email = this.state.email;
+      cv.phone = this.state.phone;
       return { cv };
+    });
+    this.setState({
+      name: "",
+      email: "",
+      phone: "",
     });
   }
 
   render() {
     return (
       <div>
-        <PersonalForm onChange={this.onChange} onClick={this.onClick} />
+        <PersonalForm
+          onChange={this.onChange}
+          onClick={this.onClick}
+          onClickEdit={this.onClickPersonalEdit}
+          onClickDelete={this.onClickPersonalDelete}
+          name={this.state.name}
+          email={this.state.email}
+          phone={this.state.phone}
+        />
         <Display name={this.state.cv} displaying="CV: " />
       </div>
     );
--- src/components/cv-form/PersonalForm.js
+++ src/components/cv-form/PersonalForm.js
@@ -7,33 +7,37 @@ class PersonalForm extends Component {
 
   render() {
     return (
-      <div>
-        <form>
-          <label>
-            Name
-            <input
-              type="text"
-              name="name"
-              onChange={this.props.onChange}
-            ></input>
-          </label>
-          <label>
-            Email
-            <input
-              type="text"
-              name="email"
-              onChange={this.props.onChange}
-            ></input>
-          </label>
-          <label>
-            Phone
-            <input
-              type="text"
-              name="phone"
-              onChange={this.props.onChange}
-            ></input>
-          </label>
+      <div className="form-container">
+        <form className="personal-form">
+          <label for="name">Name </label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            value={this.props.name}
+            onChange={this.props.onChange}
+          ></input>
+
+          <label for="email">Email </label>
+          <input
+            id="email"
+            type="text"
+            name="email"
+            value={this.props.email}
+            onChange={this.props.onChange}
+          ></input>
+
+          <label for="phone">Phone </label>
+          <input
+            type="text"
+            name="phone"
+            value={this.props.phone}
+            onChange={this.props.onChange}
+          ></input>
+
           <button onClick={this.props.onClick}>Submit</button>
+          <button onClick={this.props.onClickEdit}>Edit</button>
+          <button onClick={this.props.onClickDelete}>Delete</button>
         </form>
       </div>
     );
